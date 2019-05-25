224483d9344dc2ed6324bb36f62474712f2da9c8
Pavol Hejný <me@pavolhejny.com>
2019-05-26 00:25:13 +0200

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,13 @@
-class API {
-    url = '';
+export const api = new Api(`http://138.197.181.210:8090`);//todo to config
 
-    fetchProducts(){
+export class Api {
 
+    constructor(apiUrl){
+        this.apiUrl = apiUrl;
+    }
+
+    async _fetchResource(path){
+        const response = await fetch(`${this.apiUrl}${path}`);
+        return await response.json();
     }
 }
